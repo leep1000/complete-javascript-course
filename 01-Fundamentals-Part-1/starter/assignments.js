d73f9f693ccca117cbf665d6e89ce19557935bb1
leep1000 <leepalmerformal@gmail.com>
2023-03-14 22:40:44 +0000

    Values and Variables Assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = "England";
+let continent = "Europe";
+let population = "70000000";
+
+console.log(country, continent, population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,21 @@
 let js = 'amazing';
-      if (js === 'amazing') alert('JavaScript is FUN!');
+console.log(40 + 8 + 23 - 10);
 
-      40 + 8 + 23 - 10;
-      console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = 'Teacher';
+
