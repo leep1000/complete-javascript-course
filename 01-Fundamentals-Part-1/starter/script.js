241f85584cdaecf8c9b5cb6c4c4c4640f58de205
leep1000 <leepalmerformal@gmail.com>
2023-03-15 00:23:19 +0000

    Data Types Lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -18,4 +19,23 @@ let PI = 3.1415;
 
 let myFirstJob = "Coder";
 let myCurrentJob = 'Teacher';
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
 
