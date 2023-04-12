996a6a1eff4dd09119b5d75632dc744ff89a9b0f
leep1000 <leepalmerformal@gmail.com>
2023-04-12 21:46:14 +0100

    completed the fundamentals!

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -88,4 +88,216 @@ function checkWinner (avgDolphins, avgKoalas) {
 
 checkWinner(avgDolphins, avgKoalas);
 checkWinner(avgDolphins2, avdKoalas2);
+
+
+//Introduction to Arrays
+
+const populations = [70, 10, 13, 1330];
+
+console.log(populations.length === 4);
+
+function percentageOfWorld1(population) {
+    let worldPopulation = 7900;
+    let populationsPercent = `${(population/worldPopulation) * 100}%`
+    return populationsPercent;
+}
+
+const percentages = [
+    percentageOfWorld1(populations[0]), 
+    percentageOfWorld1(populations[1]), 
+    percentageOfWorld1(populations[2]), 
+    percentageOfWorld1(populations[3])
+];
+console.log(percentages);
+
+
+
+const neighbours = [
+    `Andorra`,
+    `Belgium`,
+    `Germany`,
+    `Italy`,
+    `Luxembourg`,
+    `Monaco`,
+    `Spain`,
+    `Switzerland`
+];
+
+neighbours.push(`Utopia`);
+console.log(neighbours);
+
+neighbours.pop(`Utopia`);
+console.log(neighbours);
+
+if (neighbours.includes(`Germany`)) {
+    console.log(`Probably a central European country :D`);
+} else {
+    console.log(`Probably not a central European country :D`);
+}
+
+neighbours[5] =  `Principality of Monaco`;
+console.log(neighbours);
+
+
+// Coding challenge #2
+
+let tip = 0;
+
+function calcTip(bill) {
+    if(bill >= 50 && bill <= 300){
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+
+console.log(calcTip(44));
+
+const bills = [125, 55, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+console.log(tips);
+
+const total = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]; 
+console.log(total);
+
+
+
+const myCountry = {
+    country: `Great Britain`,
+    capital: `London`,
+    language: `English`,
+    population: 70,
+    neighbours: [`England`, `Wales`, `Scotland`],
+    describe: function() {
+        console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`);
+    },
+    checkIsland: function() {
+    //     if (this.neighbours.length === 0) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    return this.isIsland = this.neighbours.length === 0 ? true: false;
+    }
+}
+
+// myCountry.population += 2;
+myCountry[`population`] -= 2;
+
+
+// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+myCountry.describe();
+console.log(myCountry.checkIsland());
+
+
+const mark = {
+    name : `Mark`,
+    lastName : `Miller`,
+    mass : 78,
+    height : 1.69,
+    BMI : function() {
+        return this.mass / (this.height ** 2)
+    }
+};  
+
+const john = {
+    name : `John`,
+    lastName : `Smith`,
+    mass : 92,
+    height : 1.95,
+    BMI : function() {
+        return this.mass / (this.height ** 2)
+    }
+}; 
+
+console.log(mark.BMI());
+console.log(john.BMI());
+const higherBMI = () => mark.BMI < john.BMI ? `${john.name} ${john.lastName}'s BMI (${john.BMI()}) is higher than ${mark.name} ${mark.lastName}'s (${mark.BMI()})!` : `${mark.name} ${mark.lastName}'s BMI (${mark.BMI()}) is higher than ${john.name} ${john.lastName}'s BMI (${john.BMI()})!`;
+console.log(higherBMI());
+
+    for (let i = 0; i <51; i++) {
+    console.log(`Voter number ${i} is currently voting`);
+}
+
+
+
+const populations = [70, 10, 13, 1330];
+
+function percentageOfWorld1(population) {
+    let worldPopulation = 7900;
+    let populationsPercent = population/worldPopulation * 100;
+    return populationsPercent;
+}
+let percentages2 = [];
+
+function percentageOfWorld2() {
+    for(i=0; i<populations.length; i++) {
+        let percentage = percentageOfWorld1(populations[i])
+        percentages2.push(percentage) ;
+    }
+};
+percentageOfWorld2();
+console.log(percentages2);
+
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 
+'Russia']];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    // console.log(`Neighbour ${listOfNeighbours[i]}`)
+    for (let x = 0; x < listOfNeighbours[i].length; x++) {
+        console.log(`Neighbour ${listOfNeighbours[i][x]}`);
+        
+    }
+    
+}
+
+const population = [70, 10, 13, 1330];
+
+function percentageOfWorld1(population) {
+    let worldPopulation = 7900;
+    let populationsPercent = population/worldPopulation * 100;
+    return populationsPercent;
+}
+
+let percentage3 = [];
+let i = 0;
+while(i< population.length) {
+    percentage3.push(percentageOfWorld1(population[i]));
+    i++
+};
+
+console.log(percentage3);
 */
+
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for(let i = 0; i < bill.length;i++){
+    if(bill[i] >= 50 && bill[i] <= 300){
+        smallTip = bill[i] * 0.15;
+        tips.push(smallTip);
+        totals.push(bill[i] + smallTip);
+    } else {
+        bigTip = bill[i] * 0.20;
+        tips.push(bigTip);
+        totals.push(bill[i] + bigTip);
+
+    }
+} 
+
+console.log(tips);
+console.log(totals);
+
+
+
+function calcAverage(arr) {
+    let sum = 0;
+    for(i=0; i<arr.length; i++){
+        sum += arr[i];
+    } return sum / arr.length
+};
+
+console.log(calcAverage(totals))
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -104,7 +104,7 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 }
 
 yearsUntilRetirement(1991, `Lee`);
-*/
+
 
 const friend1 = `Michael`;
 const friend2 = `Steven`;
@@ -112,3 +112,236 @@ const friend3 = `Peter`;
 
 const friends = [`Michael`, `Steven`, `Peter`];
 console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = `Jay`;
+console.log(friends);
+
+const jonas = [`Jonas`, 'Schmedtmann', 2037-1991, friends] ;
+console.log(jonas);
+
+//exercise
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[4]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[4])];
+
+
+const friends = [`Michael`, `Steven`, `Peter`];
+const newLength = friends.push(`Jay`);
+console.log(friends);
+console.log(newLength);
+
+// To add to beginning of array
+
+friends.shift('John');
+console.log(friends);
+
+// To remove elements from arrays
+friends.pop();
+const popped = friends.pop(); // remove last
+console.log(popped);
+console.log(friends);
+
+friends.shift(); //removes first 
+console.log(friends);
+
+console.log(friends.indexOf(`Steven`)); // returns index number
+console.log(friends.indexOf(`Bob`)); // returns -1, as Bob is not in the index
+
+console.log(friends.includes(`Steven`)); // returns true
+console.log(friends.includes(`Bob`)); // returns false 
+
+if (friends.includes(`Steven`)) {
+    console.log(`You have a friend called Peter`);
+}
+
+
+const jonas = {
+    // these curly braces define a new object 
+    firstName: 'Jonas', 
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: `teacher`,
+    friends: [`Michael`, `Peter`, `Steven`]
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas[`firstName`]);
+
+const nameKey = `Name`;
+console.log(jonas[`first` + nameKey]);
+console.log(jonas[`last` + nameKey]);
+
+const interestedIn = prompt(`What do you want to know about Jonas? Choose between firstName, lastname, age, job or friends`);
+// console.log(jonas.interestedIn); // This returns undefined
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]); //dynamically accesses the interestedIn property
+} else {
+    console.log(`Wrong request! Choose between firstName, lastname, age, job or friends`);   
+}
+
+// These add properties and values to the object
+jonas.location = `Portugal`;
+jonas[`twitter`] = `@jonasschmedtman`;
+
+jonas.bestFriend = `Michael`;
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.bestFriend[0]}`);
+
+
+
+const jonas = {
+    // these curly braces define a new object 
+    firstName: 'Jonas', 
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    birthYear: 1991,
+    job: `teacher`,
+    friends: [`Michael`, `Peter`, `Steven`],
+    hasDriversLicense: true,
+    //function value
+    // calcAge: function(birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function() {
+    //     // console.log(this);
+    //     return 2037 - this.birthYear;
+    //     }
+    //     // this keyword refers to the object that the method is called on
+    //     //In this case, this refers to the jonas object
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? `a` : `no`} driver's license.`
+    }
+};
+
+console.log(jonas.calcAge(1991));
+// what appears before .calcAge() is the object that the method is called on
+
+ console.log(jonas.getSummary());
+
+console.log(`Lifting weights repetitions 1`);
+
+// for loop keeping running while condition is true
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+
+const jonas = [
+    'Jonas', 
+    'Schmedtmann',
+    2037 - 1991,
+    `teacher`,
+    [`Michael`, `Peter`, `Steven`]
+];
+
+let types = [];
+for( let i=0;i<jonas.length ; i++) {
+    // reading from jonas array
+    console.log(jonas[i], typeof jonas[i]);
+
+    //filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [
+    1991,
+    2007,
+    1969,
+    2020
+];
+
+const ages = [];
+
+for(let i=0; i<years.length; i++) {
+    ages.push(2037 - years[i]);
+    console.log(ages);
+}
+
+// continue and break 
+console.log(`---only strings---`);
+for( let i=0;i<jonas.length ; i++) {
+// we skip everything that is not a string
+    if(typeof jonas[i] !== `string`) continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+    // this only prints strings
+}
+
+console.log(`---BREAK WITH NUMBER---`);
+for(let i=0;i<jonas.length ; i++) {
+// we skip everything that is not a string
+    if(typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+    // this only prints strings
+}
+
+const jonas = [
+    'Jonas', 
+    'Schmedtmann',
+    2037 - 1991,
+    `teacher`,
+    [`Michael`, `Peter`, `Steven`]
+];
+
+//previously we looped from the beginning to the end of the array
+
+for(let i= jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+};
+
+for (let exercise = 1; exercise < 4; exercise++){
+    console.log(`--------------- Starting exercise ${exercise}`);
+
+    for(let rep=1; rep < 6; rep++){
+        console.log(`Lifting weight repetition ${rep}`);
+    }
+}
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// };
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+};
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+}
