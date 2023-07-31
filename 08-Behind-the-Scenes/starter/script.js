f6e687fc19df8885041e2a49403900b835832f0a
leep1000 <leepalmerformal@gmail.com>
2023-07-31 23:49:14 +0100

    being th scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -68,7 +68,6 @@ const addExpr = function (a, b) {
 
 var addArrow = (a, b) => a + b;
 
-*/
 
 //////////////////////////////
 // this keywords
@@ -99,4 +98,117 @@ matilda.calcAge = jonas.calcAge;
 matilda.calcAge();
 
 const f = jonas.calcAge;
-f();
+f(); // Since f does not have a year, it tries to look for it in the global object, which returns undefined
+
+///////////////////////////////
+// Regular Functions vs Arrow Functions
+
+// var firstname = `Matilda`;
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution #1 - pre-ES6
+    //   const self = this; // self or that are most common
+    //   const isMillenial = function () {
+    //     console.log(self);
+    //     console.log(self.year >= 1981 && self.year <= 1996);
+    //   };
+    //   isMillenial();
+    // },
+
+    // Solution #2 - Arrow function
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`hey ${this.firstname}`);
+  },
+};
+
+jonas.greet(); // `hey undefined` as arrow function has no this keyword of its own
+
+jonas.calcAge();
+
+////////////////////////////
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 1, 4); // Can have more arguments than parameters
+
+const addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5); // arguments keyword does no exist in arrow functions
+
+///////////////////////////////////////
+// Objects vs Primitives
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age, oldAge); // age = 31, oldAge = 30
+
+const me = {
+  name: `jonas`,
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log(me.age, friend.age); // me.age = 27, friend.age = 27
+
+*/
+
+// Primitive Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName, oldLastName);
+
+// Reference Types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica; // Actually copying the reference, which will point to the same object
+marriedJessica.lastName = 'Davis';
+// What needs to be const in const, is the value in the call stack
+
+// marriedJessica = {}; // Won't work as it will point to a new address in the heap
+
+// Copying Objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+// Object.assign merges two objects and returns a new one, creates a shallow copy
+// Only copies the properties at the first level, and won't copy nested objects (e.g. arrays)
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
+
+// For a true copy, use external libraries
